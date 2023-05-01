35b388f4e5a5214fc66b2e198f1136a9fa853073
SanjotD <112981867+SanjotD@users.noreply.github.com>
2023-05-01 11:19:42 -0600

    start

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 // USER LOGIN / SIGNUP
 
 // HTML VARIABLES
-let signInBtn = document.getElementById('sign-in-btn');
-let signUpBtn = document.getElementById('sign-up-btn');
+let signInBtn = document.getElementById("sign-in-btn");
+let signUpBtn = document.getElementById("sign-up-btn");
 
 // SIGN UP BTN CLICKED
-signUpBtn.addEventListener('click', signUpHandler);
+signUpBtn.addEventListener("click", signUpHandler);
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+  // User Inputs
+  let usernameSignUp = document
+    .getElementById("usernameInput")
+    .value.toLowerCase();
+  let passwordSignUp = document
+    .getElementById("passwordInput")
+    .value.toLowerCase();
+  let passConfirmSignUp = document
+    .getElementById("passwordConfirm")
+    .value.toLowerCase();
+  console.log("Sign Up Btn Clicked");
+
+  if (passwordSignUp === passConfirmSignUp) {
+    //save
+    console.log("save");
+  } else {
+    alert("passwords dont match");
+  }
+
+  console.log(usernameSignUp);
+  console.log(passwordSignUp);
+  console.log(passConfirmSignUp);
 }
 
 // SIGN IN BTN CLICKED
-signInBtn.addEventListener('click', signInHandler);
+signInBtn.addEventListener("click", signInHandler);
 
 function signInHandler() {
-  console.log('Sign In Btn Clicked');
+  console.log("Sign In Btn Clicked");
 }
